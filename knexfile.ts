04afb6e643758fc6619d7586fde977d03d6ec0ca
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-02-19 18:23:09 -0300

    Instalando e Configurando Knex.js

---
+++ knexfile.ts
@@ -0,0 +1,17 @@
+export default {
+  client: "sqlite3",
+  connection: {
+    filename: "./src/database/database.db",
+  },
+  useNullAsDefault: true,
+
+  migrations: {
+    extension: "ts",
+    directory: "./src/database/migrations",
+  },
+
+  seeds: {
+    extension: "ts",
+    directory: "./src/database/seeds",
+  },
+}
