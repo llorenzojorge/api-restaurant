8bae44aff1a37f9900e282e783a33aa0de23bf8d
Lorenzo Jorge - MULTIVIX/Cachoeiro-ES <161716578+llorenzojorge@users.noreply.github.com>
2026-02-26 21:21:03 -0300

    Rota e Controller de sessão da mesa

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express"
 
 import { productsRoutes } from "./products-routes.js"
 import { tablesRoutes } from "./tables-routes.js"
+import { tablesSessionsRoutes } from "./tables-sessions-routes.js"
 
 const routes = Router()
+routes.use("/tables-sessions", tablesSessionsRoutes)
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
 
